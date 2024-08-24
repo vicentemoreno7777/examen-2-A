084416aa7c2817b04e80d57d5f04cc0a71928e06
vicentemoreno7777 <162452353+vicentemoreno7777@users.noreply.github.com>
2024-08-24 02:15:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+// Función para aceptar cookies
+function acceptCookies() {
+    document.getElementById('cookie-notice').style.display = 'none';
+}
+
+// Función para abrir el modal del carrito
+function openCart() {
+    document.getElementById('cart-modal').style.display = 'block';
+}
+
+// Función para cerrar el modal del carrito
+function closeCart() {
+    document.getElementById('cart-modal').style.display = 'none';
+}
